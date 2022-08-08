d41bc3091dc90ac496ebb758e0157f05981597a0
Ashley Gyngell <ashleygyngell@gmail.com>
2022-08-08 12:25:25 +0100

    chat app created

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'chat',
 ]
 
@@ -74,10 +75,15 @@ WSGI_APPLICATION = 'chatapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': '',
+        'PASSWORD': '',
+        'NAME': 'chatapp',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
